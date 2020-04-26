ab5672a45dee4873267fe51241d90d2aa0f53599
Piotr Krogulec <krogulec.piotr@gmail.com>
2020-04-26 10:55:23 +0200

    ex2 single responsibillity starting point

---
+++ src/main/java/info/krogulec/sda/goodpractices/solid/singleresponsibility/Email.java
@@ -0,0 +1,57 @@
+package info.krogulec.sda.goodpractices.solid.singleresponsibility;
+
+/**
+ * @author krogulecp
+ */
+//TODO Spełnić zasadę single responsibillity
+class Email {
+    private String from;
+    private String to;
+    private String subject;
+    private String message;
+
+    public void send(){
+        System.out.println("sending email");
+    }
+
+    public void validate(){
+        if (message.contains("kwiatek")){
+            System.out.println("sending not permitted");
+        } else {
+            System.out.println("email valid");
+        }
+    }
+
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
